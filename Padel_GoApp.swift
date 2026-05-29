705cc7564aaa5702ef01e796e8ba6ff49cf195c7
dzakynabil <mdzakynabil@student.ciputra.ac.id>
2026-05-29 11:43:57 +0700

    Initial Commit

---
+++ Padel_GoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Padel_GoApp.swift
+//  Padel Go
+//
+//  Created by student on 29/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Padel_GoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
