d90876a88a36a23f57a4349d5708945ca313ae19
dzakynabil <mdzakynabil@student.ciputra.ac.id>
2026-05-29 12:25:47 +0700

    add file

---
+++ Views/ExerciseGuideView.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  Padel Go
+//
+//  Created by student on 29/05/26.
+//
+
---
+++ Views/ProgressTrackingView.swift
@@ -0,0 +1,7 @@
+//
+//  ProgresTrackingView.swift
+//  Padel Go
+//
+//  Created by student on 29/05/26.
+//
+
