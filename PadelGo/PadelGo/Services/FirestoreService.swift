221bbfebdc6d9261996fb3affe9a76d8934daa20
sharonnatalies <sharonsutanto25@gmail.com>
2026-06-02 18:07:03 +0700

    fetch exercises

--- PadelGo/PadelGo/Services/FirestoreService.swift
+++ PadelGo/PadelGo/Services/FirestoreService.swift
@@ -14,7 +14,7 @@ static let shared = FirestoreService()
 
 private let db = Firestore.firestore()
 
-// MARK: - FETCH SKILLS
+    // FETCH SKILLS
 
     func fetchSkills(
         completion: @escaping ([PadelSkill]) -> Void
@@ -54,6 +54,57 @@ private let db = Firestore.firestore()
 
             }
     }
+    
+    // FETCH EXERCISES
+
+    func fetchExercises(
+        completion: @escaping ([PadelExercise]) -> Void
+    ) {
+
+        db.collection("exercises")
+            .getDocuments(source: .server) { snapshot, error in
+
+                guard let documents = snapshot?.documents else {
+
+                    completion([])
+                    return
+                }
+
+                let exercises = documents.compactMap {
+                    document -> PadelExercise? in
+
+                    let data = document.data()
+
+                    guard
+                        let id = data["id"] as? String,
+                        let name = data["name"] as? String,
+                        let description =
+                            data["description"] as? String,
+                        let steps =
+                            data["steps"] as? [String],
+                        let imageIcon =
+                            data["imageIcon"] as? String,
+                        let targetSkill =
+                            data["targetSkill"] as? String
+                    else {
+
+                        return nil
+                    }
+
+                    return PadelExercise(
+                        id: id,
+                        name: name,
+                        description: description,
+                        steps: steps,
+                        imageIcon: imageIcon,
+                        targetSkill: targetSkill
+                    )
+                }
+
+                completion(exercises)
+            }
+    }
+
     
     
 
