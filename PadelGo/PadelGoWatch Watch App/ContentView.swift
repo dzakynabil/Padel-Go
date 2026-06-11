b1b5979dbcf042806f7f72188fe3dee9a52d4db6
sfiorenzaaa <sfiorenza@student.ciputra.ac.id>
2026-06-12 01:21:23 +0700

    Add watch + contentview

---
+++ PadelGo/PadelGoWatch Watch App/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  PadelGoWatch Watch App
+//
+//  Created by Macbook on 11/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        WatchProgressView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PadelGo/PadelGoWatch Watch App/PadelGoWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PadelGoWatchApp.swift
+//  PadelGoWatch Watch App
+//
+//  Created by Macbook on 12/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PadelGoWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
